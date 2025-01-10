7fb698b1eef44d2b4da59c78fab79f15b0e3c070
Giuseppe Ussia <ussia.astro@gmail.com>
2025-01-10 20:53:19 +0100

    Removed SSL mode from database connection

--- db_connection.py
+++ db_connection.py
@@ -13,7 +13,8 @@ DB_CONFIG = {
 
 def get_timeseries_data(start_date, end_date):
     """Fetch time-series data from PostgreSQL between given dates."""
-    engine = create_engine(f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}?sslmode=require")
+    engine = create_engine(f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}")
+
     
     query = f"""
         SELECT time, wsh FROM time_series_data
