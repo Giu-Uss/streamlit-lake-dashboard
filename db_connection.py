70bccbfb854e75e09a12013c15d20b10caa8da6b
Giuseppe Ussia <ussia.astro@gmail.com>
2025-01-10 21:17:15 +0100

    Disabled SSL for database connection

--- db_connection.py
+++ db_connection.py
@@ -2,25 +2,26 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 
-# Database connection details
+# Database credentials from Streamlit secrets
 DB_CONFIG = {
-    "dbname": "streamlit_lake_db",
-    "user": "streamlit_lake_db_user",
-    "password": "NJTV4XbHoPxo4iYnrcIHOf2V3gvuuC9R",
-    "host": "dpg-cu0lj0pu0jms73dsdqh0-a",
-    "port": 5432,
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
 }
+
+# Remove SSL mode
+engine = create_engine(
+    f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}?sslmode=disable"
+)
 
 def get_timeseries_data(start_date, end_date):
     """Fetch time-series data from PostgreSQL between given dates."""
-    engine = create_engine(f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}")
-
-    
     query = f"""
         SELECT time, wsh FROM time_series_data
         WHERE time BETWEEN '{start_date}' AND '{end_date}'
     """
-    
     df = pd.read_sql(query, engine)
     return df
 
